f010c8585a14154583f47f94c737dcc7b08ed932
kmishmael <kmishmael@gmail.com>
2023-06-23 15:25:31 +0300

    fix: typo

--- opcode2.c
+++ opcode2.c
@@ -114,7 +114,7 @@ void mod(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 		free_dlistint(*stack);
 		exit(EXIT_FAILURE);
 	}
